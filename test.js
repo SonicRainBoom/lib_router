5e1a322310aad41162b793f33a225e3759bb7a5d
Hendrik 'T4cC0re' Meyer <mail@t4cc0.re>
2016-04-29 02:59:33 +0200

    [travis] added travis and simple test

---
+++ test.js
@@ -0,0 +1,24 @@
+'use strict';
+var router   = require('./index');
+var srbEvent = require('lib_srbevent');
+
+setTimeout(
+  () => {
+    console.error('timeout after 30 sec.');
+    process.exit(1);
+  }, 30000
+);
+
+srbEvent.SRBEvent.event.on(
+  'router_listening', (port)=> {
+    if (port == 12348) {
+      console.log('server listening and event fired.');
+      process.exit(0);
+    } else {
+      console.error('port differs.');
+      process.exit(1);
+    }
+  }
+);
+
+new router.Router({port: 12348}).start();
